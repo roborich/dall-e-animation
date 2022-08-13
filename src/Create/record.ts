fdbdf97ed3b4dae166d61cbd5abff69a96ca844e
Rich Howell <rich@aurorasolar.com>
2022-08-12 21:23:39 -0400

    better mime type support

--- src/Create/record.ts
+++ src/Create/record.ts
@@ -1,12 +1,30 @@
 import { AnimationState, PlayType } from "./create-types";
+const mimeTypes = [
+  "video/webm;codecs=vp9",
+  "video/webm;codecs=vp8",
+  "video/webm",
+  "video/mp4",
+];
+const mimeType: string | undefined = mimeTypes.filter((type) =>
+  MediaRecorder.isTypeSupported(type),
+)[0];
+const fileExtension = mimeType?.includes("webm") ? "webm" : "mp4";
+if (!mimeType) {
+  console.error("Your browser does not support mp4 or webm video recording");
+}
 
 export const record = (
   ctx: CanvasRenderingContext2D,
   state: AnimationState,
 ) => {
   const videoStream = ctx.canvas.captureStream(30);
+  mimeTypes.forEach((type) => {
+    const isSupported = MediaRecorder.isTypeSupported(type);
+    console.log(`${type} : ${isSupported}`);
+  });
+
   const mediaRecorder = new MediaRecorder(videoStream, {
-    mimeType: "video/webm;codecs=VP9",
+    mimeType,
     videoBitsPerSecond: 8_000_000,
     audioBitsPerSecond: 0,
   });
@@ -15,11 +33,11 @@ export const record = (
     chunks.push(e.data);
   };
   mediaRecorder.onstop = function (e) {
-    const blob = new Blob(chunks, { type: "video/webm;codecs=VP9" });
+    const blob = new Blob(chunks, { type: mimeType });
     const url = URL.createObjectURL(blob);
     const link = document.createElement("a");
     link.href = url;
-    link.download = "video.webm";
+    link.download = `${state.animationType}-${state.images.length}-images.${fileExtension}`;
     link.click();
   };
 
